8f392184a1e9792e3b3b7eaf0c1280729d31b44b
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-04-19 23:37:54 +0300

    Верстка  списка

--- frontend/src/views/Ads/AdListView.vue
+++ frontend/src/views/Ads/AdListView.vue
@@ -1,7 +1,7 @@
 <template>
     <v-container>
-        <v-row justify="center">
-            <h1>Home</h1>
+        <v-row justify="center" sm6 offset-sm3>
+            <h1 class="text--secondary mb-3 mt-3">My ads</h1>
         </v-row>
     </v-container>
 </template>
